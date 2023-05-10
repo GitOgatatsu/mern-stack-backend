ab2a2ec11c4f8469716f46fe2d51aa8357f3ad45
herasake <ogawa.ogatatsu.tatsuya.jp@gmail.com>
2023-05-10 19:26:37 +0900

    2023/05/10 19:26

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
-import express from "express";
+import express, { Request, Response } from "express";
+const app = express();
+import session from "express-session";
+import cookieParser from "cookie-parser";
 
-const app: express.Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(session({
+	secret: "secret_key",
+	resave: false,
+	saveUninitialized: false,
+	cookie: { httpOnly: true, secure: false, maxAge: 1000 * 60 * 30 }
+}));
+declare module "express-session" {
+	interface Session {
+		email: string;
+	}
+};
 
 import { connectDB } from "./utils/database";
-import { ItemModel } from "./utils/schemaModels";
+import { ItemModel, UserModel } from "./utils/schemaModels";
 
 
 
+
+//// ItemFunctions
 // CreateItem
-app.post("/item/create", async (req: express.Request, res: express.Response) => {
+app.post("/item/create", async (req: Request, res: Response) => {
 	try {
 		await connectDB();
 //		console.log(req.body.title);
@@ -24,7 +40,8 @@ app.post("/item/create", async (req: express.Request, res: express.Response) => 
 
 
 // ReadAllItems
-app.get("/", async (req: express.Request, res: express.Response) => {
+app.get("/", async (req: Request, res: Response) => {
+	console.log(req.session.email);
 	try {
 		await connectDB();
 		const allItems = await ItemModel.find();
@@ -37,7 +54,7 @@ app.get("/", async (req: express.Request, res: express.Response) => {
 
 
 // ReadItem
-app.get("/item/read/:id", async (req: express.Request, res: express.Response) => {
+app.get("/item/read/:id", async (req: Request, res: Response) => {
 	try {
 		await connectDB();
 		const singleItem = await ItemModel.findById(req.params.id);
@@ -48,6 +65,69 @@ app.get("/item/read/:id", async (req: express.Request, res: express.Response) =>
 });
 
 
+
+// UpdateItem
+app.put("/item/update/:id", async (req: Request, res: Response) => {
+	try {
+		await connectDB();
+		await ItemModel.updateOne({ _id: req.params.id }, req.body );
+		return res.status(200).json({ message: "アイテム更新成功" });
+	} catch (err) {
+		return res.status(500).json({ message: "アイテム更新失敗" });
+	}
+});
+
+
+
+// DeleteItem
+app.delete("/item/delete/:id", async (req: Request, res: Response) => {
+	try {
+		await connectDB();
+		await ItemModel.deleteOne({ _id: req.params.id });
+		return res.status(200).json({ message: "アイテム削除成功" });
+	} catch (err) {
+		return res.status(500).json({ message: "アイテム削除失敗" });
+	}
+});
+
+
+
+//// UserFunctions
+// RegisterUser
+app.post("/user/register", async (req: Request, res: Response) => {
+	try {
+		await connectDB();
+		await UserModel.create(req.body);
+		return res.status(200).json({ message: "ユーザ登録成功" });
+	} catch (err) {
+		return res.status(500).json({ message: "ユーザ登録失敗" });
+	}
+});
+
+
+// LoginUser
+app.post("/user/login", async (req: Request, res: Response) => {
+	try {
+		await connectDB();
+		const savedUserData = await UserModel.findOne({ email: req.body.email });
+		if (savedUserData) {
+			if (savedUserData.password === req.body.password) {
+				req.session.email = req.body.email;
+				return res.status(200).json({ message: "ログイン成功" });
+			} else {
+				return res.status(400).json({ message: "ログイン失敗: パスワードが違います" });
+			}
+		} else {
+			return res.status(400).json({ message: "ログイン失敗: ユーザが存在しません" });
+		}
+	} catch (err) {
+		return res.status(500).json({ message: "ログイン失敗" });
+	}
+});
+
+
+
+// Listen
 app.listen(5000, () => {
 	console.log("ポート5000番で起動しました。");
 });
--- src/utils/schemaModels.ts
+++ src/utils/schemaModels.ts
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+
+
+
+
 const ItemSchema = new Schema({
 	title: String,
 	image: String,
@@ -10,5 +14,26 @@ const ItemSchema = new Schema({
 	email: String
 });
 
+
+
+const UserSchema = new Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
+
+
+
 const ItemModel = mongoose.model("Item", ItemSchema);
-export { ItemModel };
+const UserModel = mongoose.model("User", UserSchema);
+export { ItemModel, UserModel };
